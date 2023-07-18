b45b1b113684f54b86b9462f4d48c0d1f99fc247
ekhfa <ekhfahossain@gmail.com>
2023-07-19 01:02:41 +0600

    Added Product Model

---
+++ models/productModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Enter Product Name"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Enter Price"],
+      default: 0,
+    },
+    photo: {
+      type: String,
+      required: [false, "Enter Image"],
+    },
+    description: {
+      type: String,
+      required: [false, "Enter Description"],
+    },
+    category: {
+      type: String,
+      required: [false, "Enter Category"],
+    },
+    rating: {
+      type: Number,
+      required: [false, "Enter Rating"],
+    },
+    countInStock: {
+      type: Number,
+      required: [false, "Enter Stock"],
+    },
+    numofReviews: {
+      type: Number,
+      required: [false, "Enter Reviews"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
